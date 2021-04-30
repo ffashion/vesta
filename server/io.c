1d8fdec80845fac152969755a067e876206d28f0
ffashion <1323579954@qq.com>
2021-04-30 23:37:23 +0800

    部分 framefork代码

---
+++ server/io.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+//用于处理套接字的io 传输
+
+/**
+ * io range  file write to socket 断点续传/压缩/加密
+*/
+int io_range_write2socket(int filefd,size_t start, size_t end, int socketfd){
+
+}
+
+
+
+/**
+ * io多文件写入 单个socket 文件描述符必须以-1结尾
+*/
+int static io_write2socket(int *filefd,int socketfd){
+    
+}
+
+
+/**
+ * 
+*/
+int static io_read(){
+
+}
+
+
+
+/**
+ * 
+ * 
+*/
+int static io_dir_listen(){
+
+}
---
+++ server/log.c
@@ -0,0 +1,6 @@
+//用于简短的记录日志
+int log(char *level,char *title){
+    
+}
+
+
